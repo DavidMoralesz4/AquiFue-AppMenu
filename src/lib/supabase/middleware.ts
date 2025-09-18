fcd1a091c9ffd1fc34d42b07cfe932d100000891
DavidMoralesz4 <jd3285201@gmail.com>
2025-09-18 01:37:20 -0500

    advance supabase config

---
+++ src/lib/supabase/middleware.ts
@@ -0,0 +1,7 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+import { hasEnvVars } from "../utils";
+
+
+
+
---
+++ src/lib/utils.ts
@@ -0,0 +1,5 @@
+
+
+export const hasEnvVars =
+  process.env.NEXT_PUBLIC_SUPABASE_URL &&
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
